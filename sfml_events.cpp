fb83be8e63f116138e2c7b4ddb302f1e57fe8fb8
abreathingcorpse <raul.rst11@gmail.com>
2022-12-23 14:31:20 -0600

    Fixed the TODO: When two joysticks are connected it'll switch to keyboard mode by removing a single controller

--- sfml_events.cpp
+++ sfml_events.cpp
@@ -15,6 +15,14 @@ int main() {
 	// Will become true if the game is paused
 	bool paused = false;
 
+	// Check how many Joysticks are connected, if any
+	unsigned int connected_joysticks = 0;
+	for (int i=0; i < 8; i++) {
+		if (Joystick::isConnected(i)) {
+			connected_joysticks += 1;
+		}	
+	}
+
 	while (window.isOpen()) {
 
 		// Declare the event union
@@ -23,6 +31,15 @@ int main() {
 		// while there are pending events...
 		while (window.pollEvent(event)) {
 
+			// ifs instead of a switch was used due to the
+			// event.joystickConnect.joystickId giving me the number of 
+			// the keyboard, I believe it was coming from the event.key.code
+			// According to the following link:
+			// https://www.geeksforgeeks.org/switch-vs-else/
+			// A switch is not actually ran from top to botton, I believe that's
+			// causing issues
+
+			// window closed
 			if (event.type == Event::Closed) {
 				if (paused) {
 					cout << "Removing pause." << endl;
@@ -31,11 +48,17 @@ int main() {
 				window.close();
 			}
 			
+			// TODO: For some reason whenever I close with MOD+W
+			// The Game loses focus before closing. Need to check
+			// if this is due to Qtile or this happens on Windows too.
 			if (event.type == Event::LostFocus) {
 				cout << "Pausing the Game." << endl;
 				paused = true;
 			}
 
+			// key pressed gets triggered whenever ANY key is pressed
+			// BUG:Since I moved this section to an if, it's no longer checking
+			// whether or not I press Y to confirm quitting.
 			if (event.type == Event::KeyPressed) {
 				if (!asked_quitting && event.key.code == Keyboard::Escape) {
 					cout << "Are you sure you want to quit? " << 
@@ -50,85 +73,26 @@ int main() {
 				} 
 			}
 
+			// If no Joysticks, use Keyboard Mode 
 			if (event.type == Event::JoystickDisconnected) {
-				cout << "joystick disconnected: " <<
-				event.joystickConnect.joystickId << endl;
-				cout << "Switching to Keyboard Mode." << endl;
+				connected_joysticks -= 1;
+				if (connected_joysticks < 1) {
+					cout << "Switching to Keyboard Mode." << endl;
+				}
 			}
 			if (event.type == Event::JoystickConnected) {
-					cout << "joystick connected: " <<
-					event.joystickConnect.joystickId << endl;
-					cout << "Switching to Joystick Mode." << endl;
+				cout << "Switching to Joystick Mode." << endl;
+				connected_joysticks += 1;
 			}
-			// check the type of the event...
-			/*switch (event.type) {
-
-				// window closed
-				case Event::Closed:
-					if (paused) {
-						cout << "Removing pause." << endl;
-						paused = false;
-					}
-					window.close();
-					break;
-
-				case Event::LostFocus:
-					// TODO: For some reason whenever I close with MOD+W
-					// The Game loses focus before closing. Need to check
-					// if this is due to Qtile or this happens on Windows too.
-					cout << "Pausing the Game." << endl;
-					paused = true;
-					break;
-
-				// key pressed gets triggered whenever ANY key is pressed
-				case Event::KeyPressed:
-					if (!asked_quitting && event.key.code == Keyboard::Escape) {
-						cout << "Are you sure you want to quit? " << 
-						"[Y/<Any other key>]" << endl;
-						asked_quitting = true;
-						break;
-					}
-					if (asked_quitting && event.key.code == Keyboard::Y) {
-						window.close();
-						break;
-					} else if (asked_quitting){ 
-						cout << "Unpausing the game" << endl;
-						asked_quitting = false;
-						break;
-					} 
-
-				// TODO: Currently when two joysticks are connected it'll switch
-				// to keyboard mode by removing a single controller
-				// BUG: For some reason the Event::KeyPressed is making the
-				// event.joystickConnect.joystickId is whichever key I press
-				case Event::JoystickDisconnected:
-					cout << "joystick disconnected: " <<
-					event.joystickConnect.joystickId << endl;
-					cout << "Switching to Keyboard Mode." << endl;
-					break;
-
-				case Event::JoystickConnected:
-					cout << "joystick connected: " <<
-					event.joystickConnect.joystickId << endl;
-					cout << "Switching to Joystick Mode." << endl;
-					break;
-	
-				// we don't process other types of events
-				default:
-					break;
-			}*/
 		}
-
-	}
 
 // The JoystickButtonPressed and JoystickButtonReleased events, simply print the information in
 // the event.joystickButton member.
+// It should also print the input from two Joysticks
 
 // The JoystickMoved event, simply print the information within the event.joystickMove event and
 // attempt to find a sweet spot of when the axis actually moved on a controller.
-
-// The JoystickConnected and JoystickDisconnected events, the focus should move from keyboard to
-// Joystick when it detects that one is connected. It should also print the input from two Joysticks
-// and if the player 1 removes the Joystick, the Joystick of player 2 should still be working.
+// It should also print the input from two Joysticks
+	}
 	return 0;
 }
