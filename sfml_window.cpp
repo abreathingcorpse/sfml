152d4d6687f449ac43eaf3c6f12f25229f51256b
abreathingcorpse <raul.rst11@gmail.com>
2022-12-26 17:25:20 -0600

    window.getSize() only gets called once now, instead of each iteration. The window now toggles between default and fullscreen mode

--- sfml_window.cpp
+++ sfml_window.cpp
@@ -5,17 +5,34 @@
 using namespace sf;
 using namespace std;
 
+// This is toggled between true/false whenever the player presses V
+bool fullscreen_flag = false;
+
+// Receives the referece address to window, as well as the fullscreen_flag
+// Toggles between default and Fullscreen mode
+void toggle_fullscreen(Window& window, bool fullscreen_flag) {
+	if (fullscreen_flag) {
+		VideoMode DesktopVideoMode = VideoMode::getDesktopMode();
+		window.create(DesktopVideoMode, "My window", Style::Fullscreen);
+	} else {
+		window.create(VideoMode(800,600), "My window");
+	}
+}
+
 int main() {
 	
-	Window window(VideoMode(800,600), "My window");
+	// Declare the window
+	Window window;
 
-	// I'd like to set it to Fullscreen but, this requires a valid video mode
-	// Style::Fullscreen
+	// TODO: Currently the user can't realize it's in fullscreen mode
+	// It needs to, at least, render a black color
+	toggle_fullscreen(window, fullscreen_flag);
 
-	// TODO:
-	// call: static const std::vector<VideoMode>& sf::VideoMode::getFullscreenModes	()
-	// returns: Array containing all the supported fullscreen modes
-	// How? Learn about the events, whenever the key 'f' is pressed make it fullscreen
+	// Get the size of the window 
+	// Call: Vector2u sf::Window::getSize()	const
+	// TODO: Call this again whenever the Event::Resized gets triggered
+	Vector2u size = window.getSize();
+	cout << "Original Window size: " << size.x << "," << size.y << endl;
 
 	// run the program as long as the window is open
 	while (window.isOpen()) {
@@ -24,16 +41,21 @@ int main() {
 		// the game loop
 		Event event;
 
-		// Get the size of the window every iterarion of the game loop
-		// Call: Vector2u sf::Window::getSize()	const
-		Vector2u size = window.getSize();
-		cout << "Window size: " << size.x << "," << size.y << endl;
-
 		while (window.pollEvent(event)) {
 			// "close requested" event: we close the window
 			if (event.type == Event::Closed) {
 				window.close();	
 			}
+			// The window toggles between the default and fullscreen mode
+			if (event.type == Event::KeyPressed && event.key.code == Keyboard::F) {
+				fullscreen_flag = !fullscreen_flag;
+				toggle_fullscreen(window, fullscreen_flag);
+			}
+			// TODO:
+			// call:
+			// static const std::vector<VideoMode>& VideoMode::getFullscreenModes	()
+			// returns: Array containing all the supported fullscreen modes
+			// When? whenever the key 'V'
 		}
 	}
 
